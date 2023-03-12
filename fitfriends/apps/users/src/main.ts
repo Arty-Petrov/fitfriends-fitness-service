7ae54272a1e59096d355cb5b7e826651ab34a974
Artyom Petrov <artyom.petrov@gmail.com>
2023-03-12 10:40:47 +0300

    rename auth app to users

---
+++ fitfriends/apps/users/src/main.ts
@@ -0,0 +1,17 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app/app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      skipUndefinedProperties: true,
+    })
+  );
+  await app.init();
+  Logger.log(`🚀 Users application is running`);
+}
+
+bootstrap();
