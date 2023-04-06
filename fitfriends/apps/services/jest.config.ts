4a85d779bf54023647281e2f144c1d58dada500d
Artyom Petrov <artyom.petrov@gmail.com>
2023-04-06 08:58:53 +0300

    fix: rename trainings app to services

--- fitfriends/apps/services/jest.config.ts
+++ fitfriends/apps/services/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'trainings',
+  displayName: 'services',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/trainings',
+  coverageDirectory: '../../coverage/apps/services',
 };
